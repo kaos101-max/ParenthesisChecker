91750622d8de6871da4cb6b4b2bc5edb01f0017f
kaos101-max <watanabedaisuke616@gmail.com>
2025-11-20 03:28:02 +0000

    attempt

--- Parenthesis.java
+++ Parenthesis.java
@@ -1,7 +1,19 @@
 //
 //  Class author: Daisuke Watanabe
 //  Date created:  11/13/25
-//  General description: 
+//  General description: The program will demonstrate whether or not if the parenthesis/brackets are logical.
 //
-import Java.util.Stack
 
+//precon: The parenthsis will confirmed in shuffled illogical order
+//postcon: The program will check whether or not if the brackets/parenthesis is in the right place.
+import Java.util.Stack;
+
+public class Correction {
+    public static void main(String[] args)
+    {
+        System.out.println(isValid("(]")); //false
+        System.out.println(isValid("([{({})}])")); //true
+        System.out.println(isValid("(([]){})")); //true
+        System.out.println(isValid("{}[())()(]")); //false
+    }
+}
