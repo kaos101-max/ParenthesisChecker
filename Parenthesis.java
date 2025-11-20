375dae4a5b5dfd9df5cc784fcc39fb893a019b8e
kaos101-max <watanabedaisuke616@gmail.com>
2025-11-20 18:38:41 +0000

    progress

--- Parenthesis.java
+++ Parenthesis.java
@@ -4,16 +4,22 @@
 //  General description: The program will demonstrate whether or not if the parenthesis/brackets are logical.
 //
 
-//precon: The parenthsis will confirmed in shuffled illogical order
+//precon: The 'while' and 'for' loop will run to identify the parantheses.
 //postcon: The program will check whether or not if the brackets/parenthesis is in the right place.
 import Java.util.Stack;
 
 public class Correction {
     public static void main(String[] args)
     {
-        System.out.println(isValid("(]")); //false
+        pars(isValid("(]")); //false
         System.out.println(isValid("([{({})}])")); //true
         System.out.println(isValid("(([]){})")); //true
         System.out.println(isValid("{}[())()(]")); //false
+    }
+    public static void pars(int space){
+        String first = "(]";
+        for (int i = space; i < 0; i++)
+
+            
     }
 }
